54393ef6e5d997ceca1309061e9cbfc844571d18
brok3n <limesaru@gmail.com>
2018-07-06 16:35:32 +0900

    작업시작 한시간째, UI를 다만든듯합니다

---
+++ HOOKTRIS/Assets/BrokeN/Scripts/UIController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+	public void OnObject(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+    public void OffObject(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeScene(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+}
