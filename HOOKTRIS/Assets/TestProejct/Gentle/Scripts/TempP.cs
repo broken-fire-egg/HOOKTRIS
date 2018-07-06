a67f69934724455e22df3d71c6304fb3cb63771f
Gentlet <cyc07297@gmail.com>
2018-07-06 18:29:34 +0900

    갈고리

---
+++ HOOKTRIS/Assets/TestProejct/Gentle/Scripts/TempP.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempP : MonoBehaviour
+{
+    public float speed;
+    public float power;
+    public float grabpower;
+
+    private Rigidbody2D rigid;
+    
+    [SerializeField]
+    private Hook hookprefab;
+
+    private Hook hook;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        hook = null;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 vel = rigid.velocity;
+        rigid.velocity = Vector2.zero;
+        Movement();
+
+        ThrowHook();
+        if (hook != null)
+            hook.NextUpdate();
+
+        if (rigid.velocity.x == 0)
+            rigid.velocity = new Vector2(vel.x, rigid.velocity.y);
+        if (rigid.velocity.y == 0)
+            rigid.velocity = new Vector2(rigid.velocity.x, vel.y);
+
+    }
+
+    private void Movement()
+    {
+        Vector2 move = new Vector2((Input.GetAxis("Horizontal") * speed), rigid.velocity.y);
+
+        rigid.velocity = move;
+    }
+
+    private void ThrowHook()
+    {
+        if (Input.GetMouseButtonDown(0) && hook == null)
+        {
+            Vector2 mousepos = Vector2.zero;
+
+            mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            hook = GameObject.Instantiate(hookprefab).Init(this, (mousepos - (Vector2)transform.position).normalized, power);
+            hook.transform.position = transform.position;
+        }
+        else if (Input.GetMouseButtonDown(0) && hook != null)
+        {
+            Destroy(hook.gameObject);
+            hook = null;
+        }
+    }
+
+    #region Properties
+
+    public Rigidbody2D Rigid
+    {
+        get
+        {
+            return rigid;
+        }
+
+        set
+        {
+            rigid = value;
+        }
+    }
+
+    #endregion
+}
