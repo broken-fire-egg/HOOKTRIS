2a602647f1a32fd1e65f55f2648037a4e0fed0bd
brok3n <limesaru@gmail.com>
2018-07-06 19:31:47 +0900

    ㅜㅜ

---
+++ HOOKTRIS/Assets/BrokenFireEgg/Scripts/SpineManager.cs
@@ -0,0 +1,34 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpineManager : MonoBehaviour {
+    SkeletonAnimation SA;
+
+        [SpineAnimation(dataField: "skeletonAnimation")]
+        public string die = "";
+        [SpineAnimation(dataField: "skeletonAnimation")]
+        public string idle = "";
+        [SpineAnimation(dataField: "skeletonAnimation")]
+        public string throw0 = "";
+        [SpineAnimation(dataField: "skeletonAnimation")]
+        public string throw1 = "";
+        [SpineAnimation(dataField: "skeletonAnimation")]
+        public string move = "";
+        [SpineAnimation(dataField: "skeletonAnimation")]
+        public string jump = "";
+        [SpineAnimation(dataField: "skeletonAnimation")]
+        public string clear = "";
+    
+    public void ChangeAnimation(string action,int tracknum,bool loop)
+    {
+        SA.AnimationState.SetAnimation(tracknum, action, loop);
+
+    }
+    public void Start()
+    {
+        SA = GetComponent<SkeletonAnimation>();
+        Debug.Log(move);
+    }
+}
