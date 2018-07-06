9231ea864090f4f636973b77c5e4a08ea10113e9
brok3n <limesaru@gmail.com>
2018-07-07 04:21:02 +0900

    카메라 따라가기 & 맵엔딩 구멍만듬

---
+++ HOOKTRIS/Assets/BrokeN/Scripts/CameraFollower.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public GameObject player;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2f, -10f);
+    }
+}
